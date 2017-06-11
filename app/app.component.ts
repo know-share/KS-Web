7c613cce7fe3c5377c4db025c18ac6eb6ed38913
minmiguelm <cmiguelmg@gmail.com>
2017-06-11 15:32:08 -0500

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-app',
+    template: `<h1>Testing</h1>`
+    //templateUrl: '',
+    //styleUrls: ['']
+})
+export class AppComponent{
+}
