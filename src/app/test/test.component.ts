796c3956decf0dbb87fa5674517329e05ed47fa4
minmiguelm <minmiguelm>
2017-07-27 13:16:55 -0500

    ajuste un poco responsive al affix

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,6 +7,9 @@ import { DOCUMENT } from '@angular/platform-browser';
 export class ScrollPointDirective {
     
     @Input() scrollPoint: number;
+    originalWidth: string;
+    scrollMin:number = -1;
+    active:boolean = false;
 
     constructor(
         @Inject(DOCUMENT) private document: Document,
@@ -17,15 +20,34 @@ export class ScrollPointDirective {
     @HostListener('window:scroll', [])
     onWindowScroll() {
         const windowScroll = this.document.body.scrollTop;
-        if (windowScroll > this.scrollPoint) {
+        let topLimit = this.el.nativeElement.getBoundingClientRect().top;
+        // if (windowScroll > this.scrollPoint) {
+        if(topLimit <= 60 && !this.active){
             //add class to the native element
-            this.renderer.setElementClass(this.el.nativeElement, 'sticky-nav-basis', true);
-            this.el.nativeElement.style.width = '1px';
+            this.settingSticky(true);
+            if(this.scrollMin == -1){
+                this.scrollMin = windowScroll;
+                this.active = true;
+            }
+        } else if(this.scrollMin != -1 && windowScroll > this.scrollMin){
+            this.settingSticky(true);
         } else {
             //remove class from native element
-            this.renderer.setElementClass(this.el.nativeElement, 'sticky-nav-basis', false);
+            this.settingSticky(false);
         }
-
     }
 
+    settingSticky(value){
+        this.renderer.setElementClass(this.el.nativeElement, 'sticky-nav', value);
+        if(value){
+            this.el.nativeElement.style.width = this.originalWidth+'px';
+            this.el.nativeElement.style.top = '60px';
+        }else{
+            if(this.active)
+                this.el.nativeElement.style.width = '100%';
+            this.originalWidth = this.el.nativeElement.offsetWidth;
+            this.active = false;
+            this.scrollMin = -1;
+        }
+    }
 }
