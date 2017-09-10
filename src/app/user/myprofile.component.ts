a76aac82486275ec8420099b813f16c145ab5822
pabloaGaitan <pabloagaitanb@hotmail.com>
2017-09-10 17:10:56 -0500

    busqueda de idea para continuar arreglo de like coloreado, crear idea mi perfil y centrado de detalles

--- src/app/user/myprofile.component.ts
+++ src/app/user/myprofile.component.ts
@@ -57,7 +57,7 @@ export class ProfileComponent implements OnInit {
     areasConocimientoSeg: AreaConocimiento[] = [];
 
     idea: Idea = new Idea;
-    tg: TrabajoGrado;
+    tg: TrabajoGrado = new TrabajoGrado();
     valid: boolean = true;
     ideasPro: Array<Idea> = new Array;
     ideas: Array<Idea> = new Array;
@@ -68,6 +68,7 @@ export class ProfileComponent implements OnInit {
     tags: Array<Tag> = new Array;
     selectedTags: any[]
     filteredTagsMultiple: any[];
+    
 
     selectedValueTipo: string;
     contenido: string;
